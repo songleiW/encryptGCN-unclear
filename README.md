cdbaa697e66e058eb3e8820af23814263d953d42
songleiW <1466160618@qq.com>
2024-10-17 20:01:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# encryptGCN
+ 
---
+++ cmake-build-debug/CMakeFiles/encryptGCN.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encryptGCN.dir/main.cpp.o"
+  "CMakeFiles/encryptGCN.dir/main.cpp.o.d"
+  "CMakeFiles/encryptGCN.dir/mpcUtil.cpp.o"
+  "CMakeFiles/encryptGCN.dir/mpcUtil.cpp.o.d"
+  "CMakeFiles/encryptGCN.dir/test.cpp.o"
+  "CMakeFiles/encryptGCN.dir/test.cpp.o.d"
+  "CMakeFiles/encryptGCN.dir/util.cpp.o"
+  "CMakeFiles/encryptGCN.dir/util.cpp.o.d"
+  "encryptGCN"
+  "encryptGCN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encryptGCN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
